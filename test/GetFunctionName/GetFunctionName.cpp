15810c64a4b9a98af83de1aa759b56209c6939cf
skyel1u <ohmysky111@gmail.com>
2018-08-20 09:59:53 +0800

    first pass:GetFunctionName

---
+++ test/GetFunctionName/GetFunctionName.cpp
@@ -0,0 +1,27 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace
+{
+
+struct Test : public FunctionPass
+{
+    static char ID;
+
+    Test() : FunctionPass(ID) { ; }
+
+    virtual bool runOnFunction(Function &function)
+    {
+        errs() << "Function name:";
+        errs().write_escaped(function.getName()) << "\n";
+        return false;
+    }
+};
+
+} // namespace
+
+char Test::ID = 0;
+static RegisterPass<Test> X("function-name", "FunctionName.", false, false);
