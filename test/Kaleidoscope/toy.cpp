95ec199e1f6ece2602bef718b8358b4ff5627397
skyel1u <ohmysky111@gmail.com>
2018-10-18 10:22:45 +0800

    finished AST namespace

--- test/Kaleidoscope/toy.cpp
+++ test/Kaleidoscope/toy.cpp
@@ -83,7 +83,65 @@ static int gettok() {
     }
     
 }
+//===----------------------------------------------------------------------===//
+// Abstract Syntax Tree (aka Parse Tree)
+//===----------------------------------------------------------------------===//
+namespace {
+/// ExprAST - Base class for all expression nodes.
+class ExprAST
+{
+  public:
+    virtual ~ExprAST() {}
+    };
+};
 
+/// NumberExprAST - Expression class for numeric literals like "1.0".
+class NumberExprAST : public ExprAST {
+    public:
+        NumberExprAST(double val) {}
+};
+
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+    std::string Name;
+    public:
+        VariableExprAST(const std::string &name) : Name(name) {}
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+    public:
+        BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs) {}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+    std::string Callee;
+    std::vector<ExprAST*> Args;
+    public:
+        CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
+        : Callee(callee), Args(args) {}
+};
+
+/// PrototypeAST - captures its name, argument names.
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+    public:
+        PrototypeAST(const std::string &name, const std::vector<std::string> &args)
+        : Name(name), Args(args) {}
+};
+
+/// FunctionAST - define a function
+class FunctionAST {
+    public:
+        FunctionAST(PrototypeAST *proto, ExprAST *body) {}
+};
+///-----end namespace------------
+
+//===----------------------------
+// Main function
+//===----------------------------
 int main() {
     return 0;
 }
